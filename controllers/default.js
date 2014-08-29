2e4fa1fb22f586bb8a6c385b4b063f2443e88a6b
Paulo McNally <paulomcnally@gmail.com>
2014-08-28 21:23:55 -0600

    implement log security

--- controllers/default.js
+++ controllers/default.js
@@ -7,47 +7,67 @@ var apiKey = process.env.MAILGUN_KEY;
 //Your domain, from the Mailgun Control Panel
 var domain = process.env.MAILGUN_DOMAIN;
 
-module.exports = function(req, res) {
+var limit = 5;
 
+module.exports = function(req, res) {
+  // find customer
   models.customers.findById(req.params.id, function(err, customer) {
     if (err) {
       res.json({error:{message:err}});
     }
     else {
       if (customer) {
-        var from = req.body.from;
-        var to = customer.to;
-        var subject = req.body.subject;
-        var message = req.body.message;
+        var now = new Date();
+        // get count log by client_id, ip address and date
+        models.logs.count({
+          'client_id': req.params.id,
+          'ip': req.heroku.ip,
+          'created_at': {
+            $lte: now
+          }
+        }, function(err, count) {
+          if (err) {
+            res.json({error:{message:err}});
+          }
+          else if (count <= limit) {
+            var from = req.body.from;
+            var to = customer.to;
+            var subject = req.body.subject;
+            var message = req.body.message;
 
-        var mailgun = new Mailgun({apiKey: apiKey, domain: domain});
+            var mailgun = new Mailgun({apiKey: apiKey, domain: domain});
 
-        var data = {
-          from: from,
-          to: to,
-          subject: subject,
-          html: message
-        };
+            var data = {
+              from: from,
+              to: to,
+              subject: subject,
+              html: message
+            };
 
-        mailgun.messages().send(data, function(err, body) {
-          var log = new models.logs;
-          log.from = from;
-          log.subject = subject;
-          log.message = message;
-          log.status = (err) ? true : false;
-          log.save(function(error) {
-            if (error) {
-              res.json({error:{message:error.message}});
-            }
-            else if (err) {
-              res.json({error:{message:err.message}});
-            }
-            else {
-              res.json({response:'Mensaje enviado'});
-            }
-          });
+            mailgun.messages().send(data, function(err, body) {
+              var log = new models.logs;
+              log.from = from;
+              log.subject = subject;
+              log.message = message;
+              log.status = (err) ? true : false;
+              log.ip = req.heroku.ip;
+              log.save(function(error) {
+                if (error) {
+                  res.json({error:{message:error.message}});
+                }
+                else if (err) {
+                  res.json({error:{message:err.message}});
+                }
+                else {
+                  res.json({response:'Mensaje enviado'});
+                }
+              });
+            });
+          }
+          else {
+            res.json({error:{message:'No puedes enviar más de ' + limit + ' mensajes por día.'}});
+          }
         });
-
       }
       else {
         res.json({error:{message:'El usuario no existe'}});
